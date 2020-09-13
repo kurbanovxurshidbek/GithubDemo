0a44c6095a8d452d8cfecf05f0768fc67af0c51c
Khurshid <shamsun.com@gmail.com>
2020-09-13 11:02:31 +0900

    Initial Commit

---
+++ GithubDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by KHURSHIDBEK on 2020/09/13.
+//  Copyright © 2020 MySoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
